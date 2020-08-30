17b223261349ad85a161eb625b13a057a88cab74
ArmerZ <50855686+armernuntiya@users.noreply.github.com>
2020-08-30 16:24:13 +0700

    Update register.php

--- dbweb/register.php
+++ dbweb/register.php
@@ -10,28 +10,19 @@
 <link rel='stylesheet' href='style.css'></head>
 
 <body>
-    <h2>Register</h2>
     <form action="registerdb.php" method="POST">
         <table border="1">
+        <tr>
+        <td><h2>Register</h2>
+        </td>
+    </tr>
             <tr>
-            <div class='input'>
-                <td>UserID : 
-                <input type="text" name="UserID">
-            </div>
-            </td>
-            </tr>
-            <tr>
-            <div class='input'>
-                <td>Username : 
-                <input type="text" name="Username">
-            </div>
-            </td>
-            </tr>
-            <tr>
-            <td>
-            <div class='input'>
-                    Password : 
-            <input type="text" name="Password"></div></td>
+                <td> 
+                <input type="text" name="UserID" placeholder="UserID">
+                <p>
+                <input type="text" name="Username" placeholder="username">
+                <p>
+                <input type="text" name="Password" placeholder="password"></td>
             </tr>
             <tr>
                 <td align="center"><input type="submit" name="reg_user" value="complete"><br>
@@ -41,7 +32,7 @@
         </form>
         <?php 
    if(isset($_SESSION['error'])){
-    echo "<p align='center' font-color='red'>".$_SESSION['error'];
+    echo "<p>".$_SESSION['error'];
     unset($_SESSION['error']);
     }?>
 </body>
